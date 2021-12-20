53eddccce3c86678764d7af5e528d815aea6e026
chitomo12 <mrk3_12@yahoo.co.jp>
2021-12-20 20:03:19 +0900

    Initial Commit

---
+++ WaveAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaveAnimation
+//
+//  Created by 福田正知 on 2021/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaveAnimation/WaveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAnimationApp.swift
+//  WaveAnimation
+//
+//  Created by 福田正知 on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WaveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
